714b5683423b8f97e9cd30b4d49f9a951caeb08b
smartuser460 <kashish.sharma@smartcoin.co.in>
2023-06-09 01:12:45 +0530

    initialised blog-api project in springboot

---
+++ src/test/java/com/kashish/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashish.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
